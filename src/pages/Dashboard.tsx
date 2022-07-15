81a5ad936b6f2d32dda000fe7a64b540d41f9b69
Travis Luckenbaugh <tlucke@subaru.com>
2022-07-15 13:38:54 -0400

    Start dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,12 +3,19 @@ import { View } from 'react-native';
 import { setItem } from '../stores/Local';
 import { MyLinkButton } from '../components/MyButton';
 import { MyText } from '../components/MyText';
+import { setSession } from '../stores/Session';
+import { MyStyleSheet } from '../components/MyStyles';
 
 // TODO: Dashboard
 
 export const Dashboard = () => {
-    return <View style={{flex: 1, alignItems: 'center', justifyContent:'center'}}>
-        <MyLinkButton onPress={() => setItem("appState", "login")} title= "< Login"></MyLinkButton>
+    const logout = () => {
+        setItem("appState", "login");
+        setSession(undefined);
+    }
+
+    return <View style={MyStyleSheet.screen}>
+        <MyLinkButton onPress={logout} title= "< Login"></MyLinkButton>
         <MyText>Device Registered :: Proceed to App</MyText>
     </View>
 }
